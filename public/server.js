5d6fe115afe1248518111e76d2dc615324d0bcdf
Moises <moises-vd@hotmail.com>
2023-01-01 15:37:45 -0300

    add setup to setupServer

---
+++ public/server.js
@@ -0,0 +1,17 @@
+const cors = require('cors');
+const { obtenerPost, agregarPost } = require('./db')
+const express = require('express');
+const app = express();
+
+app.use(cors());
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.listen(3000, console.log("SERVIDOR ENCENDIDO"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
